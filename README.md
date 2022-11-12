25ffc754a0a8c14f5b57b5fcccc9ad6abd262fd5
Shubham Dohare <sdohare11@gmail.com>
2022-11-12 12:42:07 +0530

    added code

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # learning_containers_using_go
 
+Only works on Linux system
+
 Learning and following repo
 
 - https://github.com/lizrice/containers-from-scratch
+
+```bash
+go run main.go run /bin/bash
+
+```
---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+    "fmt",
+    "io/ioutil",
+    "os",
+    "os/exec",
+    "path/filepath",
+    "strconv",
+    "syscall",
+)
+
+func main(){
+    switch os.Args[1] {
+    case "run":
+        run()
+    case "child":
+        child()
+    default:
+        panic("Use run or child to run the program `go run main.go run /bin/bash`")
+    }
+}
+
+func run(){
+    fmt.Printf("Running %v \n", os.Args[2:])
+    cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...),...)
+    cmd.Stdin = os.Stdin
+    cmd.Stdout = os.Stdout
+    cmd.Stderr = os.Stderr
+    cmd.SysProcAttr = &syscall.SysprocAttr{
+        Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+        Unshareflags : syscall.CLONE_NEWNS,
+    }
+    must(cmd.Run())
+}
+
+func child(){
+    fmt.Printf("Running %v \n", os.Args[3:]...)
+    cg()
+    cmd := exec.Command(os.Args[2], os.Args[3:]...)
+    cmd.Stdin = os.Stdin
+    cmd.Stdout = os.Stdout
+    cmd.Stderr = os.Stderr
+
+    must(syscall.Sethostname([]byte("container")))
+    must(syscall.Chroot("/home/ubuntu/ubuntufs"))
+    must(os.Chdir("/"))
+    must(syscall.Mount("proc", "proc", "proc", 0, ""))
+    must(syscall.Mount("thing", "mytemp", "tmpds", 0, ""))
+    must(cmd.Run())
+    must(syscall.Ubmount("proc", 0))
+    must(syscall.Ubmount("thing", 0))
+}
+
+func cg(){
+    cgroups := "/sys/fs/cgroup/"
+    pids := filepath.Join(cgroups, "pids")
+    os.Mkdir(filepath.Join(pids, "dore"), 0755)
+    must(ioutil.WriteFile(filepath.Join(pids, "dore/pids.max"), []byte("20"), 0700))
+    // Removes the new cgroup in place after the container exits
+    must(ioutil.WriteFile(filepath.Join(pids, "dore/notify_on_release"), []byte("1"), 0700))
+    must(ioutil.WriteFile(filepath.Join(pids, "dore/cgroup.rocs"), []byte(strconv.Itoa(os.Getpid())), 0700))
+}
+
+func must(err error){
+    if err != nil {
+        paic(err)
+    }
+}
