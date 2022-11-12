a6139944cfb4cfbc483dd9c750d62767c5f924b9
Shubham Dohare <sdohare11@gmail.com>
2022-11-12 13:29:53 +0530

    added code

--- main.go
+++ main.go
@@ -21,46 +21,53 @@ func main(){
     }
 }
 
-func run(){
+
+func run() {
     fmt.Printf("Running %v \n", os.Args[2:])
-    cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...),...)
+
+    cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
     cmd.Stdin = os.Stdin
     cmd.Stdout = os.Stdout
     cmd.Stderr = os.Stderr
-    cmd.SysProcAttr = &syscall.SysprocAttr{
-        Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
-        Unshareflags : syscall.CLONE_NEWNS,
+    cmd.SysProcAttr = &syscall.SysProcAttr{
+            Cloneflags:   syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+            Unshareflags: syscall.CLONE_NEWNS,
     }
+
     must(cmd.Run())
 }
 
-func child(){
-    fmt.Printf("Running %v \n", os.Args[3:]...)
-    cg()
+func child() {
+    fmt.Printf("Running %v \n", os.Args[2:])
     cmd := exec.Command(os.Args[2], os.Args[3:]...)
     cmd.Stdin = os.Stdin
     cmd.Stdout = os.Stdout
     cmd.Stderr = os.Stderr
 
     must(syscall.Sethostname([]byte("container")))
-    must(syscall.Chroot("/home/ubuntu/ubuntufs"))
+    must(syscall.Chroot("/home/ubuntu/ubuntuFileDir"))
     must(os.Chdir("/"))
     must(syscall.Mount("proc", "proc", "proc", 0, ""))
-    must(syscall.Mount("thing", "mytemp", "tmpds", 0, ""))
+    must(syscall.Mount("thing", "mytemp", "tmpfs", 0, ""))
+
+    cg()
+
     must(cmd.Run())
-    must(syscall.Ubmount("proc", 0))
-    must(syscall.Ubmount("thing", 0))
+
+    must(syscall.Unmount("proc", 0))
+    must(syscall.Unmount("thing", 0))
 }
 
-func cg(){
+func cg() {
     cgroups := "/sys/fs/cgroup/"
     pids := filepath.Join(cgroups, "pids")
-    os.Mkdir(filepath.Join(pids, "dore"), 0755)
-    must(ioutil.WriteFile(filepath.Join(pids, "dore/pids.max"), []byte("20"), 0700))
+    os.MkdirAll(filepath.Join(pids, "testing"), 0755)
+    must(ioutil.WriteFile(filepath.Join(pids, "testing/pids.max"), []byte("20"), 0700))
     // Removes the new cgroup in place after the container exits
-    must(ioutil.WriteFile(filepath.Join(pids, "dore/notify_on_release"), []byte("1"), 0700))
-    must(ioutil.WriteFile(filepath.Join(pids, "dore/cgroup.rocs"), []byte(strconv.Itoa(os.Getpid())), 0700))
+    must(ioutil.WriteFile(filepath.Join(pids, "testing/notify_on_release"), []byte("1"), 0700))
+    must(ioutil.WriteFile(filepath.Join(pids, "testing/cgroup.procs"), []byte(strconv.Itoa(os.Getpid())), 0700))
 }
+
 
 func must(err error){
     if err != nil {
